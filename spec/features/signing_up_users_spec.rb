0485872d63fa0f3d8823a45b3c3531a0d6fa9fce
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-06-27 18:20:48 +0200

    Set up user sign-up, rspec test for failing sign up incomplete

---
+++ spec/features/signing_up_users_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "SignUp Users" do
+  scenario "A user signs up with valid credentials" do
+    visit "/"
+    click_link "Sign up"
+
+    fill_in "Email",	with: "user@example.com"
+    fill_in "Password", with: "123456"
+    fill_in "Password confirmation", with: "123456"
+    click_button "Sign up"
+
+    expect(page).to have_content("Welcome! You have signed up successfully")
+    
+  end
+
+  scenario "A user fails to sign up due to invalid credentials" do
+    visit "/"
+    click_link "Sign up"
+
+    fill_in "Email",	with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_button "Sign up"
+
+    # expect(page).to have_content("You have not signed up successfully")
+    
+  end
+end
