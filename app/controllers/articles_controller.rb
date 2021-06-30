859d159a441f69ddbd465884a04e72676b5e61c6
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-07-01 00:22:32 +0200

    restricted access to edit and update action for arcticles on controller level

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,15 +21,25 @@ class ArticlesController < ApplicationController
   def show
   end
   def edit
+    unless @article.user == current_user
+      # raise
+      flash[:alert] = "You are not owner of this article."
+      redirect_to root_path
+    end
   end
   def update
     # raise
-    if @article.update(article_params)
-      flash[:success] = "Article has been updated"
-      redirect_to article_path(@article)
+    unless @article.user == current_user
+      flash[:alert] = "You are not owner of this article."
+      redirect_to root_path
     else
-      flash.now[:danger] = "Article has not been updated"
-      render :edit
+      if @article.update(article_params)
+        flash[:success] = "Article has been updated"
+        redirect_to article_path(@article)
+      else
+        flash.now[:danger] = "Article has not been updated"
+        render :edit
+      end
     end
   end
   def destroy
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe "Articles", type: :request do
 
   before do
-    @article = Article.create(title: "An Article" , body: "This is an article")
+    @john = User.create(email: "john@example.com", password: "123456")
+    @james = User.create(email: "james@example.com", password: "123456")
+    @article = Article.create!(title: "An Article" , body: "This is an article", user: @john)
   end
 
   describe "GET /articles" do
@@ -16,6 +18,7 @@ RSpec.describe "Articles", type: :request do
   describe "GET /articles/:id" do
     context "with existing article" do
       it "handles existing article" do
+        # puts @article.id
         get "/articles/#{@article.id}"
         expect(response).to have_http_status(:success)
       end
@@ -23,12 +26,46 @@ RSpec.describe "Articles", type: :request do
     context "with not existing article" do
       it "handles not existing article" do
         get "/articles/xxxx"
+        # puts @request.host
+        # puts response.headers
         message = "Article not found"
         expect(flash[:alert]).to eq(message)
         expect(response).to have_http_status(302)
       end
     end
 
+    describe "Get /articles/:id/edit" do
+      context "with NOT signed in user" do
+        before { get "/articles/#{@article.id}/edit" }
+
+        it "redirects to sign in page" do
+          expect(response.status).to eq 302
+          message = "You need to sign in or sign up before continuing."
+          expect(flash[:alert]).to eq(message)
+        end
+      end
+      context "with signed in user who is owner" do
+        before do
+          login_as(@john)
+          get "/articles/#{@article.id}/edit"
+        end
+        it "successfully requests edit page" do
+          expect(response.status).to eq 200
+        end
+      end
+
+      context "with signed in user who is NOT owner" do
+        before do
+          login_as(@james)
+          get "/articles/#{@article.id}/edit"
+        end
+        it "redirects to the home page" do
+          expect(response.status).to eq 302
+          message = "You are not owner of this article."
+          expect(flash[:alert]).to eq(message)
+        end
+      end
+    end
 
   end
 end
