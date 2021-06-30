fd9f23fbc0485a9261a4adaf435b4573bbffb408
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-06-30 15:38:28 +0200

    restricted access to New Article link on articles index page

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -2,15 +2,14 @@ require "rails_helper"
 
 RSpec.feature "Listing Articles" do
   before do
-    john = User.create!(email: "john@example.com", password: "123456")
+    @john = User.create!(email: "john@example.com", password: "123456")
     james = User.create!(email: "james@example.com", password: "123456")
-    @article1 = Article.create(title: "The first article" , body: "Body of first article", user: john)
+    @article1 = Article.create(title: "The first article" , body: "Body of first article", user: @john)
     @article2 = Article.create(title: "The second article", body: "Body of second article", user: james)
   end
 
-  scenario "A user lists all articles" do
+  scenario "A user lists all articles with articles created and user is not signed in" do
     visit "/"
-
     expect(page).to have_content(@article1.title)
     expect(page).to have_content(@article1.body)
     expect(page).to have_content(@article2.title)
@@ -19,7 +18,23 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content("Created by: #{@article2.user.email}")
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
   end
+
+  scenario "A user lists all articles with articles created and user is signed in" do
+    login_as(@john)
+    visit "/"
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_content("Created by: #{@article1.user.email}")
+    expect(page).to have_content("Created by: #{@article2.user.email}")
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+  end
+
   scenario "A user has no articles" do
     Article.destroy_all
     visit "/"
